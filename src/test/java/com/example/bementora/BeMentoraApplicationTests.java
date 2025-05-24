303ff563ab68fb16e95fc9c08ecaad8e7d109bc4
Elliot Nguyen <nguyenduykiet72@gmail.com>
2025-05-24 22:08:10 +0700

    init base BE Mentora Java

---
+++ src/test/java/com/example/bementora/BeMentoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bementora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeMentoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
