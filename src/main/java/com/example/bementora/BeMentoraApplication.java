29551ee04b2288a12d6ed249df789c2018ec645f
Elliot Nguyen <nguyenduykiet72@gmail.com>
2025-10-09 15:31:02 +0700

    test gpg key

--- src/main/java/com/example/bementora/BeMentoraApplication.java
+++ src/main/java/com/example/bementora/BeMentoraApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BeMentoraApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BeMentoraApplication.class, args);
+        System.out.println("Hello World!");
     }
 
 }
